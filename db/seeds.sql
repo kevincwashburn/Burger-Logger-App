2cc37be9594cd2bb0accb18ff17ed39d997e9aea
Kevin Washburn <kevincwashburn@gmail.com>
2020-03-12 09:50:30 -0400

    none

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- SELECT * FROM burgers_db.burgers;
+
+INSERT INTO burgers (name) VALUES ('cheeseburger');
+INSERT INTO burgers (name) VALUES ('breakfast burger');
+INSERT INTO burgers (name) VALUES ('bison burger');
+
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "burgers_db"
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+app.post("/api/burgers", function (req, res) {
+  connection.query("INSERT INTO burgers SET ?",
+    [req.body],
+    function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+    
+});
+
+app.get("/", function (req, res) {
+  connection.query("SELECT * FROM burgers", function (err, results) {
+    if (err) throw err;
+
+    res.render("index", { boigahs: results });
+  });
+});
+
+app.put("/api/burgers/:id", function (req, res) {
+  connection.query("UPDATE burgers SET devoured = true WHERE id = ?", 
+  [req.params.id], 
+
+  function (err, result) {
+    console.log(req.params.id);
+    if (err) throw err;
+    else if (result.changedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+
+
+// next step: devoured boolean button
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
